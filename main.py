aa6dc0cbf75bc9493fdd44d4e2b3b8295ded8ef4
Varshni-twks <varshni.n@thoughtworks.com>
2025-03-14 23:35:39 +0530

    add persistence through file storage.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typer import Typer
-from student import Students,Student
+from student import Students, Student
 
-app= Typer()
+app = Typer()
 
 students_object = Students()
+
+
 
 @app.command()
 def show():
@@ -12,13 +14,15 @@ def show():
     for student in students:
         print(f"{student.roll_no} \t | {student.name} \t | {student.standard} \t |")
 
+
 @app.command()
-def get(roll_no : str):
+def get(roll_no: str):
     student = students_object.get(roll_no)
     if student:
         print(f"{student.roll_no} \t | {student.name} \t | {student.standard} \t |")
     else:
         print("No student found with given roll Number")
+
 
 @app.command()
 def add():
@@ -26,8 +30,8 @@ def add():
     roll_no = input("Enter student roll number:")
     standard = input("Enter standard(I to XII):")
 
-    students_object.add(Student(roll_no=roll_no,name=name,standard=standard))
-    
+    students_object.add(Student(roll_no=roll_no, name=name, standard=standard))
+
 
 if __name__ == "__main__":
     app()
--- student.py
+++ student.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import json
+import os
 
-
+DATA_FILE = "students.json"
 @dataclass
 class Student:
     roll_no: str
@@ -10,6 +12,7 @@ class Student:
 class Students:
     def __init__(self) -> None:
         self.students: list[Student] = []
+        self.load_data()
 
     def show(self) -> list[Student]:
         print(len(self.students))
@@ -24,17 +27,35 @@ class Students:
     def add(self, student: Student) -> None:
         self.students.append(student)
         print(self.students)
+        self.save_data()
 
     def delete(self, roll_no:str) -> None:
         student = self.get(roll_no)
         if student:
             self.students.remove(student)
+            self.save_data()
 
     def update(self, new_student : Student) -> None:
-        for student in self.students:
+        for i,student in enumerate(self.students):
             if student.roll_no == new_student.roll_no:
-                student = new_student
+                self.students[i] = new_student
+                self.save_data()
                 break
+
+    def load_data(self):
+        if os.path.exists(DATA_FILE):
+            try:
+                with open(DATA_FILE,'r') as f:
+                    students_dict = json.load(f)
+                self.students = [Student(**student) for student in students_dict]
+            except json.JSONDecodeError:
+                self.students = []
+    
+    def save_data(self):
+        students_dict = [asdict(student) for student in self.students]
+        with open(DATA_FILE, 'w') as f:
+            json.dump(students_dict,f)
+    
     
 
     
